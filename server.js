de46907d2d3d2cddda7272586c0c7375673a93a7
Shea Ketsdever <sheaketsdever@Sheas-MBP.wireless.yale.internal>
2018-11-08 13:55:49 -0500

    Edit port to work with heroku

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ app.use(express.static('./')); //so that root displays index.html
 app.use(express.json());
 
 app.get('/', (request, response) => {
-  //displays index.html
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Express running');
 });
