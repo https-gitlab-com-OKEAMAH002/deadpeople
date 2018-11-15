b8c78010567e34f85509aad9be8aa1a86a15e46b
Shea Ketsdever <sheaketsdever@Sheas-MBP.wireless.yale.internal>
2018-11-15 14:56:08 -0500

    Add redirect

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('/db-init-tables', async (req, res) => {
 		await client.query('CREATE TABLE landmarks(object_id INTEGER PRIMARY KEY, description VARCHAR(500), has_photos BOOLEAN);');
 		client.release();
 		console.log("Initialized tables");
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -36,6 +37,7 @@ app.get(RegExp('/db-delete-table/(objects|graves|landmarks)'), async (req, res) 
 		await client.query('DROP TABLE ' + req.originalUrl.substring(String("/db-delete-table/").length) + ';');
 		client.release();
 		console.log("Deleted table: ", req.originalUrl.substring(String("/db-delete-table/").length));
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -48,6 +50,7 @@ app.get('/db-add-object', async (req, res) => {
 		await client.query('INSERT INTO objects(object_id, type, location) VALUES(DEFAULT, \'' + req.query.type + '\', \'' + req.query.location + '\');');		
 		client.release();
 		console.log("Added object for query: ", req.query);
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -60,6 +63,7 @@ app.get('/db-delete-object', async (req, res) => {
 		await client.query('DELETE FROM objects WHERE location=\'' + req.query.location + '\' AND type=\'' + req.query.type + '\';');		
 		client.release();
 		console.log("Deleted object for query: ", req.query);
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -72,6 +76,7 @@ app.get('/db-view-objects', async (req, res) => {
 		let query = await client.query('SELECT * FROM objects;');
 		console.log("Current objects: ", query.rows);
 		client.release();
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -87,6 +92,7 @@ app.get('/db-test-create', async (req, res) => {
 		await client.query('INSERT INTO test_table VALUES(1, \'hello\');');
 		client.release();
 		console.log("created test_table");
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -99,6 +105,7 @@ app.get('/db-test-log', async (req, res) => {
 		const result = await client.query('SELECT * FROM test_table;');
 		console.log("logging test_table: ", result);
 		client.release();
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -111,6 +118,7 @@ app.get('/db-test-delete', async (req, res) => {
 		const query = await client.query('DROP TABLE test_table;');
 		client.release();
 		console.log("deleted test_table");
+		res.redirect("/");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
