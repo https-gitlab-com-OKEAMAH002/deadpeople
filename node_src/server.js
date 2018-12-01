65a3914e12a46eae208e2d283a42f0a6767d6943
Shea Ketsdever <sheaketsdever@Sheas-MBP.wireless.yale.internal>
2018-12-01 17:46:13 -0500

    Upload locations data to database

--- node_src/server.js
+++ node_src/server.js
@@ -13,6 +13,7 @@ const pool = new Pool({
 	connectionString: dburl,
 	ssl: true
 });
+const plot_coordinates = require('./plot_coordinates_sql');
 
 /*
  *	App configuration
@@ -70,6 +71,7 @@ app.get('/db-init-tables', async (req, res) => {
 		await client.query('CREATE TABLE objects(object_id SERIAL PRIMARY KEY, type VARCHAR(40), location VARCHAR(40));');
 		await client.query('CREATE TABLE graves(object_id INTEGER PRIMARY KEY, last_name VARCHAR(40), first_name VARCHAR(40), middle_name VARCHAR(40), date_of_birth DATE, date_of_death DATE, date_of_burrial DATE, is_notable BOOLEAN);');
 		await client.query('CREATE TABLE landmarks(object_id INTEGER PRIMARY KEY, description VARCHAR(500), has_photos BOOLEAN);');
+		await client.query('CREATE TABLE locations(plot_number INTEGER PRIMARY KEY, relative_x FLOAT, relative_y FLOAT);');
 		client.release();
 		console.log("Initialized tables");
 		res.redirect("/");
@@ -79,8 +81,22 @@ app.get('/db-init-tables', async (req, res) => {
     }
 });
 
+// ADMIN ONLY -- ONE-TIME INITIALIZATION OF PLOTS / LOCATIONS
+app.get('/db-init-locations', async (req, res) => {
+	try {
+		const client = await pool.connect();
+		await client.query(plot_coordinates.getPlotCoordinates);
+		client.release();
+		console.log("Initialized locations table");
+		res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+});
+
 // ADMIN ONLY -- PERMANENTLY DELETE ONE OF THE TABLES
-app.get(RegExp('/db-delete-table/(objects|graves|landmarks)'), async (req, res) => {
+app.get(RegExp('/db-delete-table/(objects|graves|landmarks|locations)'), async (req, res) => {
 	try {
 		const client = await pool.connect();
 		await client.query('DROP TABLE ' + req.originalUrl.substring(String("/db-delete-table/").length) + ';');
