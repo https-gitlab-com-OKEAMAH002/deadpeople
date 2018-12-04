5f295306d591c4cebe820c717b78da7c964efd87
Elle Buellesbach <Elle@Elles-MBP.wireless.yale.internal>
2018-12-04 14:18:13 -0500

    Background image

--- vue_src/src/App.vue
+++ vue_src/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <Header/>
+<!--     <b-modal id="welcome" size="lg" title="Welcome">
+      <Welcome/>
+    </b-modal> -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Welcome from './components/Welcome.vue';
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Welcome
   }
 }
 </script>
@@ -24,5 +29,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 30px;
+  /*background: url(images/bg.jpg) no-repeat center center fixed; */
+  background-color:#D3D3D3;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
 }
 </style>
